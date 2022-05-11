1dec292b25bda07622df74cac7a8e9943fa84a73
Geethika2001 <71515326+Geethika2001@users.noreply.github.com>
2022-05-11 11:58:55 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { About } from './components/About';
+import React from "react";
+import { BrowserRouter ,Route, Routes } from "react-router-dom";
+// export default function About () {
+//   return (
+//     <Router>
+//       <About/>     
+//     </Router>
+//   )
+// }
 function App() {
   return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/about' element={<About/>}></Route>
+    </Routes>
+    </BrowserRouter>
 
-    <div className="App">
-      <About/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
   );
 }
 
 export default App;
+
+
